23ab8b57ca07855d18449ce532b95103f6af8dba
Dmitry Lozhkin <31573763+Coder911th@users.noreply.github.com>
2018-04-06 21:32:56 +0300

    Рефакторинг

--- server/dns-promise.js
+++ server/dns-promise.js
@@ -1,15 +1,12 @@
 const
-    dns = require('dns');
+  dns = require('dns');
 
 module.exports = function(domain) {
-    return new Promise((resolve, reject) => {
-        dns.lookup(domain, (error, address, family) =>
-            error ?
-                reject()
-            :
-                resolve({
-                    address, family
-                })
-        );
-    });
+  return new Promise((resolve, reject) => {
+    dns.lookup(domain, (error, address, family) =>
+      error
+        ? reject()
+        : resolve({ address, family })
+    );
+  });
 };
